73d84f67053707c84a3c0586d3661c3114ed13b2
BamBamLol <82470343+bambamlolol@users.noreply.github.com>
2024-02-26 09:41:46 -0600

    Create FunnyNotes.hx

---
+++ custom scripts/FunnyNotes.hx
@@ -0,0 +1,11 @@
+var floatshit:Float = 0;
+
+function update(elapsed) {
+        floatshit += 0.1;
+        for (strumline in strumLines) {
+                for (strum in strumlines) {
+                        strum.y += Math.sin(floatshit);
+                        strum.x += Math.sin(floatshit);
+                }
+        }
+}
