92ae40cc699a8ff74cdd6b39feeb4c6a72bf8205
BamBamLol <82470343+bambamlolol@users.noreply.github.com>
2024-02-26 21:14:25 -0600

    Update FunnyNotes.hx

--- custom scripts/FunnyNotes.hx
+++ custom scripts/FunnyNotes.hx
@@ -3,7 +3,7 @@ var floatshit:Float = 0;
 function update(elapsed) {
         floatshit += 0.1;
         for (strumline in strumLines) {
-                for (strum in strumlines) {
+                for (strum in strumline) {
                         strum.y += Math.sin(floatshit);
                         strum.x += Math.sin(floatshit);
                 }
